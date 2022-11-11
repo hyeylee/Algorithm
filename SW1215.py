68ab0569f81d95ba85149e21ab99d7ba559997ca
leehyeyoung <leehyeyoung9812@gmail.com>
2022-11-12 01:39:45 +0900

    .

--- SW1215.py
+++ SW1215.py
@@ -6,10 +6,10 @@ for tc in range(1,11):
     for i in range(len(arr)):
         for j in range(9-N):
             slice_str = arr[i][j:j+N]
-            reversed_arr = slice_str[::-1]
-            if(slice_str == reversed_arr):
+            reversed_arr = slice_str[::-1] # 거꾸로 뒤집기
+            if(slice_str == reversed_arr): # 두개 비교해서 회문 찾기
                 count += 1
-        for z in range(8): 
+        for z in range(8): # 세로 찾기 위해서 행 열 바꾸기
             slice_str2 = arr[z][i]
             result += slice_str2
     slice_str3 = list(map(''.join, zip(*[iter(result)]*8)))
